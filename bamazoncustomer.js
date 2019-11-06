8011df0f4d386dc434e81e7d3b69bd4691b92c58
Alex Pham <925workk@gmail.com>
2019-11-05 23:34:36 -0700

    added json and required packages

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "local host",
+    host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "",
@@ -12,8 +12,66 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     start();
-})
+});
 
-function start(){
-    
-}
+function start() {
+    connection.query("SELECT * FROM products", function(err, results) {
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: "id-selection",
+            type: "rawlist",
+            choices: function() {
+              var idArray = [];
+              for (var i = 0; i < results.length; i++) {
+                idArray.push(results[i].product_name);
+              }
+              return idArray;
+            },
+            message: "What would you like to buy?"
+          },
+          {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+          }
+        ])
+        .then(function(answer) {
+          // get the information of the chosen item
+          var chosenItem;
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].product_name === answer.id_selection) {
+              chosenItem = results[i];
+              console.log(chosenItem);
+            }
+          }
+  
+          // determine if bid was high enough
+          if (chosenItem.highest_bid < parseInt(answer.bid)) {
+            // bid was high enough, so update db, let the user know, and start over
+            connection.query(
+              "UPDATE auctions SET ? WHERE ?",
+              [
+                {
+                  highest_bid: answer.bid
+                },
+                {
+                  id: chosenItem.id
+                }
+              ],
+              function(error) {
+                if (error) throw err;
+                console.log("Bid placed successfully!");
+                start();
+              }
+            );
+          }
+          else {
+            // bid wasn't high enough, so apologize and start over
+            console.log("Your bid was too low. Try again...");
+            start();
+          }
+        });
+    });
+  }
